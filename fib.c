b49e875e0a348dd6da87a535d52c378060c1b1f7
harrisonthayer <harrisonthayer04@gmail.com>
2024-09-16 16:15:23 -0700

    Fixed the code version

--- fib.c
+++ fib.c
@@ -14,48 +14,10 @@ Program description:
 #include <stdlib.h> // atoi()
 #include <string.h>  // strcmp()
 
-unsigned long long int *memorizedFibonacciNumbersArray;
-
 /*
- Function readFileInteger takes one input,
- a string, and returns an integer. The function
- opens the file specified by the input string,
- and pulls an integer from the file to return.
-
-*/
-
-int readFileInteger(char *fileName){
-   FILE *myFile;                           // Create the file stream
-   myFile = fopen(fileName, "r");          // Open the file for reading only
-
-   int fileReadNumber;              
-   fscanf(myFile, "%d", &fileReadNumber);  // Read the integer from the file
-   fclose(myFile);                         // Close the file
-
-   return fileReadNumber;
-}
-
-/*
- Function fibRecursivelyWrapper takes in 1
+ Function fibRecursively takes in 1
  integer, and calculates the Nth fibonacci
  number recursively.
-*/
-
-unsigned long long int fibRecursivelyWrapper(unsigned long long int number){
-   if(number < 1){
-      return 0;
-   }
-   if(number == 1){
-      return 1;
-   }
-   return fibRecursively(number-1) + fibRecursively(number-2);
-}
-
-/*
- Function fibRecursivelyWrapper takes in 1
- integer, and calculates the Nth fibonacci
- number recursively using memoization to speed
- up the process.
 */
 
 unsigned long long int fibRecursively(unsigned long long int number){
@@ -65,16 +27,8 @@ unsigned long long int fibRecursively(unsigned long long int number){
    if(number == 1){
       return 1;
    }
-   if(memorizedFibonacciNumbersArray[number] != -1){
-      return memorizedFibonacciNumbersArray[number];
-   }
    return fibRecursively(number-1) + fibRecursively(number-2);
 }
-
-
-
-
-
 
 /*
  Function fibIteratively takes in 1
@@ -82,13 +36,13 @@ unsigned long long int fibRecursively(unsigned long long int number){
  number iteratively.
 */
 
-unsigned long long int fibIterativelyWrapper(int number){
+unsigned long long int fibIteratively(int number){
    // Calculate the actual Fibonacci number,
    // final answer will be stored in "c" when the loop finishes.
    unsigned long long int nMinusTwo = 0;
    unsigned long long int nMinusOne = 1; 
    unsigned long long int fibonacci;
-    for (int i = 0; i < number ; i++) {
+    for (int counter = 0; counter < number ; counter++) {
         fibonacci = nMinusTwo + nMinusOne; 
         nMinusTwo = nMinusOne;
         nMinusOne = fibonacci;
@@ -100,18 +54,16 @@ unsigned long long int fibIterativelyWrapper(int number){
 int main(int argc, char *argv[]) {
    int inputNumber = atoi(argv[1]);
    char *rORi = argv[2];
-   char *inputFileName = argv[3];
 
    // Read the number in the file, calculate
    // what fib number we will be going to...
-   int N = readFileInteger(inputFileName) + inputNumber;
    unsigned long long int computedFibNumber;
    if(strcmp(rORi,"i") == 0){
-      computedFibNumber = fibIteratively(N-2);
+      computedFibNumber = fibIteratively(inputNumber-1);
       printf("%llu\n", computedFibNumber);
    }
    if(strcmp(rORi,"r") == 0){
-      computedFibNumber = fibRecursively(N-1);
+      computedFibNumber = fibRecursively(inputNumber-1);
       printf("%llu\n", computedFibNumber);
    }
    return computedFibNumber;
